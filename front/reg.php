a9bdec9f7b8b39b636e38a32ebe6f724fb149841
rudy870103 <favoriteinfinite@gmail.com>
2023-12-29 14:02:22 +0800

    建立會員註冊表單內容檢查機制

--- front/reg.php
+++ front/reg.php
@@ -12,16 +12,44 @@
         </tr>
         <tr>
             <td class="clo">Step3:再次確認密碼</td>
-            <td><input type="text" name="pw2" id="pw2"></td>
+            <td><input type="password" name="pw2" id="pw2"></td>
         </tr>
         <tr>
             <td class="clo">Step4:信箱(忘記密碼時使用)</td>
             <td><input type="text" name="email" id="email"></td>
         </tr>
         <tr>
-            <td><input type="submit" value="註冊">
-            <input type="reset" value="清除"></td>
+            <td><input type="button" value="註冊" onclick="reg()">
+                <input type="reset" value="清除">
+            </td>
             <td></td>
         </tr>
     </table>
 </fieldset>
+<script>
+    function reg() {
+        let user = {
+            acc: $("#acc").val(),
+            pw: $("#pw").val(),
+            pw2: $("#pw2").val(),
+            email: $("#email").val()
+        }
+        if(user.acc!='' && user.pw!='' && user.pw2!=''&& user.email!=''){
+            if(user.pw == user.pw2){
+                 $.post("./api/chk_acc.php",{acc:user.acc},(res)=>{
+                    console.log(res)
+                    if(parseInt(res)==1){
+                        alert("帳號重複");
+                    }else{
+
+                    }
+                })
+            }else{
+               alert("密碼輸入不同");
+            }
+        }else{
+            alert("不可空白");
+        }
+        console.log(user);
+    }
+</script>

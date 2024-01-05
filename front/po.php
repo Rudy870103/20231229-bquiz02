5e9ecf6104c22db406d3aed93821347bf6d6bfba
rudy870103 <favoriteinfinite@gmail.com>
2024-01-05 16:06:48 +0800

    使用ajax來載入分類網誌的標題

--- front/po.php
+++ front/po.php
@@ -14,10 +14,10 @@
 <div class="nav">目前位置:首頁>分類網誌><span class="type">健康新知</span></div>
 <fieldset class="types">
     <legend>分類網誌</legend>
-    <a class="type-item">健康新知</a>
-    <a class="type-item">菸害防治</a>
-    <a class="type-item">癌症防治</a>
-    <a class="type-item">慢性病防治</a>
+    <a class="type-item" data-id="1">健康新知</a>
+    <a class="type-item" data-id="2">菸害防治</a>
+    <a class="type-item" data-id="3">癌症防治</a>
+    <a class="type-item" data-id="4">慢性病防治</a>
 </fieldset>
 <fieldset class="news-list">
     <legend>文章列表</legend>
@@ -25,9 +25,17 @@
     <div class="article"></div>
 </fieldset>
 <script>
+    getList(1)
 $(".type-item").on('click',function(){
     $(".type").text($(this).text())
+    let tyoe=$(this).data('id')
+    getList(type)
 })
+function getList(type){
+    $.get("./api/get_list.php",{type},(list)=>{
+        $(".list-items").html(list)
+    })
+}
 </script>
   
 

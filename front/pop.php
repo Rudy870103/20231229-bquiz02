19f3b4bc8df62dc50d92a46915ce51be2a8276e9
rudy870103 <favoriteinfinite@gmail.com>
2024-01-08 09:51:01 +0800

    在最新文章和人氣文章製作分頁功能

--- front/pop.php
+++ front/pop.php
@@ -7,16 +7,42 @@
             <th>人氣</th>
         </tr>
         <?php
-        $rows=$News->all(['sh'=>1]," order by `good` desc");
-        foreach($rows as $row){
+        $total = $News->count();
+        $div = 5;
+        $pages = ceil($total / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
+        $rows = $News->all(['sh' => 1], " order by `good` desc limit $start,$div");
+        foreach ($rows as $row) {
         ?>
-        <tr>
-            <td><?=$row['title'];?></td>
-            <td><?=mb_substr($row['news'],0,25);?>...</td>
-            <td></td>
-        </tr>
+            <tr>
+                <td><?= $row['title']; ?></td>
+                <td><?= mb_substr($row['news'], 0, 25); ?>...</td>
+                <td></td>
+            </tr>
         <?php
         }
         ?>
     </table>
+    <div>
+        <?php
+        if ($now - 1 > 0) {
+            $prev = $now - 1;
+            echo "<a href='index.php?do=pop&p=$prev'> ";
+            echo " < ";
+            echo " </a>";
+        }
+        for ($i = 1; $i <= $pages; $i++) {
+            $size = ($i == $now) ? 'font-size:22px;' : 'font-size:16px;';
+            echo "<a href='index.php?do=pop&p=$i' style='{$size}'> ";
+            echo $i;
+            echo " </a>";
+        }
+        if ($now + 1 <= $pages) {
+            $next = $now + 1;
+            echo "<a href='index.php?do=pop&p=$next'> ";
+            echo " > ";
+            echo " </a>";
+        }
+        ?>
 </fieldset>
